8d93fcedbf1dddea08788ab751240c05ab6aa3a8
hchenfred <hchen.fred@gmail.com>
2017-03-01 15:19:34 -0800

    Passed first test. Sending html to the browser.

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,17 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (req.method === 'GET') {
+    fs.readFile(archive.paths.siteAssets + '/index.html', (err, html) => {
+      if (err) {
+        throw err;
+      }
+      res.write(html);
+      res.end();
+    });
+  }
+  // res.end(archive.paths.list);
 };
